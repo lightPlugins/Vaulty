e8b15908d2c606816a4ecb6a65737f2f3f08b40b
lightplugins <lightningdesign@outloo.de>
2024-03-17 16:15:39 +0100

    Initial commit

---
+++ src/main/java/io/lightplugins/vaulty/Vaulty.java
@@ -0,0 +1,33 @@
+package io.lightplugins.vaulty;
+
+import org.bukkit.plugin.ServicesManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Vaulty extends JavaPlugin {
+
+    public Vaulty plugin;
+    private ServicesManager servicesManager;
+
+    @Override
+    public void onLoad() {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        servicesManager = getServer().getServicesManager();
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Hello world!");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/io/lightplugins/vaulty/VaultyResponse.java
@@ -0,0 +1,50 @@
+package io.lightplugins.vaulty;
+
+/**
+ * @param amount       Amount modified by calling method
+ * @param balance      New balance of account
+ * @param type         Success or failure of call. Using Enum of ResponseType to determine valid
+ *                     outcomes
+ * @param errorMessage Error message if the variable 'type' is ResponseType.FAILURE
+ */
+public record VaultyResponse(double amount, double balance, ResponseType type, String errorMessage) {
+
+    /**
+     * Enum for types of Responses indicating the status of a method call.
+     */
+    public static enum ResponseType {
+        SUCCESS(1),
+        FAILURE(2),
+        NOT_IMPLEMENTED(3);
+
+        private int id;
+
+        ResponseType(int id) {
+            this.id = id;
+        }
+
+        int getId() {
+            return id;
+        }
+    }
+
+    /**
+     * Constructor for EconomyResponse
+     *
+     * @param amount       Amount modified during operation
+     * @param balance      New balance of an account
+     * @param type         Success or failure type of the operation
+     * @param errorMessage Error message if necessary (commonly null)
+     */
+    public VaultyResponse {
+    }
+
+    /**
+     * Checks if an operation was successful
+     *
+     * @return Value
+     */
+    public boolean transactionSuccess() {
+        return type == ResponseType.SUCCESS;
+    }
+}
